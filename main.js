7d4d16d5b985550f4f41810c90aa7dae0976c8b8
Garrett Estrin <garrett.estrin@gmail.com>
2016-08-23 18:35:56 -0700

    want to share

--- main.js
+++ main.js
@@ -34,7 +34,12 @@ operators.forEach(function(el){
   el.addEventListener('click', function(){
     if(readOut.innerText == 0){
       // do nothing
-    } else {
+    } else if (expression.length == 2) {
+      expression[2] = readOut.innerText;
+      evaluate();
+      expression[0] = readOut.innerText;
+    }
+    else {
       expression[0] = readOut.innerText;
       readOut.innerText = this.innerText;
 
@@ -45,17 +50,42 @@ operators.forEach(function(el){
 var evaluate = function(){
   expression[2] = readOut.innerText;
   if(expression[1] == '+'){
-    readOut.innerText = Number(expression[0]) + Number(expression[2]);
+    var result = Number(expression[0]) + Number(expression[2]);
+    if(result.length > 8){
+      console.log('result is more than 9 characters');
+      readOut.innerText = Number(result).toExponential(3);
+    }else {
+      readOut.innerText = result;
+    }
     expression = [];
   } else if (expression[1] == '-'){
+    var result = Number(expression[0]) - Number(expression[2]);
+    if(result.length > 8){
+      readOut.innerText = Number(result).toExponential(3);
+    }else{
     readOut.innerText = Number(expression[0]) - Number(expression[2]);
-    expression = [];
+  } expression = [];
   } else if (expression[1] == 'X'){
+    var result = Number(expression[0]) * Number(expression[2]);
+    if(result.length > 8){
+      readOut.innerText = Number(result).toExponential(3);
+    }else {
     readOut.innerText = Number(expression[0]) * Number(expression[2]);
-    expression = [];
+  } expression = [];
   } else if (expression[1] == '/'){
+    var result = Number(expression[0]) / Number(expression[2]);
+    if(result.length > 8){
+      readOut.innerText = Number(result).toExponential(3);
+    }else {
     readOut.innerText = Number(expression[0]) / Number(expression[2]);
-    expression = [];
+  } expression = [];
+  } else if (expression[1] == '%'){
+    var result = Number(expression[2]) / Number(expression[0]);
+    if(result.length > 8) {
+      readOut.innerText = Number(result).toExponential(3);
+    }else {
+    readOut.innerText = Number(expression[2]) / Number(expression[0]);
+  } expression = [];
   } else {
     console.log('need more functions')
   }
@@ -80,5 +110,7 @@ var makeNeg = function(){
 
 // Listener for equal button
 equalButton.addEventListener('click', evaluate);
+// Listener for clear button
 clearButton.addEventListener('click', clearCal);
+// listener for negative button
 negativeButton.addEventListener('click', makeNeg);
